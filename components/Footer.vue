2b8f73e7d1c2192dbddb5a9c0ff8ea3457f41b01
unknown <kuishang@outlook.com>
2018-02-13 18:44:14 +0800

    add style

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,35 @@
 <template>
-  <footer>
-    im your footer~ welcome
+  <footer class="footer-wrap">
+    <span class="coyprights" v-html="content">
+      
+    </span>
   </footer>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        content: 'Copyrights © 2018 <a style="color:#64b5f6" href="https://github.com/KuiShang" target="_blank">Kui.Shang</a> All Rights Reserved.'
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+.footer-wrap {
+    padding: 50px 20px 30px;
+    color: #9eabb3;
+    line-height: 1.5;
+    text-align: center;
+    font-size: 1.4rem;
+    .coyprights {
+
+    }
+
+  a, a:active, a:hover, a:visited {
+    cursor: pointer;
+    color: #64b5f6;
+    text-decoration: none;
+  }
+}
+</style>
+
--- service/ArticleService.js
+++ service/ArticleService.js
@@ -1,13 +1,13 @@
 import dao from './BaseDao'
 export default {
   list (currentPage, pageSize, params) {
-    return dao.page('v6/article/list', currentPage, pageSize, params)
+    return dao.page('v1/article', currentPage, pageSize, params)
   },
   get (id) {
     return dao.get('article/detail/' + id)
   },
   getRender (id) {
-    return dao.get('v6/article/render/' + id)
+    return dao.get('v1/article/' + id)
   },
   allNames () {
     return dao.get('article/article-map')
