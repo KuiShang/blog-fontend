00cd8d116bd5964e1880babf38a167179bf3ef74
KuiShang <kuishang@outlook.com>
2018-03-07 09:42:53 +0800

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <footer>
-    im your footer~ welcome
-  </footer>
+<footer id="footer" class="main-content-wrap">
+    <span class="copyrights">
+        Copyrights © 2018 <a href="http://www.maxzhang.com">KK Shang</a>. All Rights Reserved.
+    </span>
+</footer>
 </template>
